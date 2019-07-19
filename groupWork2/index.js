c82f80ea1e84f0480e0cc534254b269dfa2b2928
Yurii Levchenko <36551537+FyuroTEX@users.noreply.github.com>
2019-07-19 20:11:35 +0300

    Add files via upload

---
+++ groupWork2/index.js
@@ -0,0 +1,21 @@
+// let prgInput = document.querySelectorAll('.prg');
+// console.log(prgInput);
+// prgInput.forEa
+// let prgInput = document.querySelectorAll("form .prg");
+// function buildPgram() {
+  
+        
+//     console.log(prgInput);
+// }
+// prgInput.forEach(items => {
+//     items.addEventListener('change', event => {
+        
+//     })
+// })
+let sideA = document.getElementById('prgAside').addEventListener('change', event => {
+    console.log(event.target.value);
+    
+    const aSide = event.target.value;
+    // alert('Horray! Someone wrote "' + this.value + '"!');
+});
+console.log(sideA);
